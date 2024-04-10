46ef54dfbc08d19482f92ce2416dd250ad91bb75
elijahred23 <elijahred23@gmail.com>
2024-04-09 22:17:46 -0500

    Eli-6: Added endpoints to grab specific users books authors checkouts

--- library-api/library_db.js
+++ library-api/library_db.js
@@ -47,6 +47,88 @@ const getUsers = async () => {
 const getCheckouts = async () => {
     return await executeLibraryDBQuery("SELECT * FROM Checkouts;");
 }
+
+const updateAuthor = async (id, name) => {
+  const query = `UPDATE Authors SET name = ${name} WHERE id = ${id}`;
+  await executeLibraryDBQuery(query);
+};
+
+const updateBook = async (id, title, authorId, ISBN, availableQuantity) => {
+  const query = `UPDATE Books SET title = ${title}, author_id = ${authorId}, ISBN = ${ISBN}, available_quantity = ${availableQuantity} WHERE id = ${id}`;
+  await executeLibraryDBQuery(query);
+};
+
+const updateUser = async (id, username, email) => {
+  const query = `UPDATE Users SET username = ${username}, email = ${email} WHERE id = ${id}`;
+  await executeLibraryDBQuery(query);
+};
+
+const updateCheckout = async (id, bookId, userId, checkoutDate, dueDate) => {
+  const query = `UPDATE Checkouts SET book_id = ${bookId}, user_id = ${userId}, checkout_date = ${checkoutDate}, due_date = ${dueDate} WHERE id = ${id}`;
+  await executeLibraryDBQuery(query);
+};
+const deleteAuthor = async (id) => {
+  const query = `DELETE FROM Authors WHERE id = ${id}`;
+  await executeLibraryDBQuery(query);
+};
+
+const deleteBook = async (id) => {
+  const query = `DELETE FROM Books WHERE id = ${id}`;
+  await executeLibraryDBQuery(query);
+};
+
+const deleteUser = async (id) => {
+  const query = `DELETE FROM Users WHERE id = ${id}`;
+  await executeLibraryDBQuery(query, [id]);
+};
+
+const deleteCheckout = async (id) => {
+  const query = `DELETE FROM Checkouts WHERE id = ${id}`;
+  await executeLibraryDBQuery(query);
+};
+// Get a specific author by ID
+const getAuthorById = async (id) => {
+  const query = `SELECT * FROM Authors WHERE id = ${id}`;
+  const results = await executeLibraryDBQuery(query);
+  return results.length > 0 ? results[0] : null;
+};
+
+// Get a specific book by ID
+const getBookById = async (id) => {
+  const query = `SELECT * FROM Books WHERE id = ${id}`;
+  const results = await executeLibraryDBQuery(query);
+  return results.length > 0 ? results[0] : null;
+};
+
+// Get a specific user by ID
+const getUserById = async (id) => {
+  const query = `SELECT * FROM Users WHERE id = ${id}`;
+  const results = await executeLibraryDBQuery(query);
+  return results.length > 0 ? results[0] : null;
+};
+
+// Get a specific checkout by ID
+const getCheckoutById = async (id) => {
+  const query = `SELECT * FROM Checkouts WHERE id = ${id}`;
+  const results = await executeLibraryDBQuery(query);
+  return results.length > 0 ? results[0] : null;
+};
+
+// Check if a book is available (available_quantity > 0)
+const isBookAvailable = async (bookId) => {
+  const query = `SELECT available_quantity FROM Books WHERE id = ${bookId}`;
+  const results = await executeLibraryDBQuery(query);
+  return results.length > 0 && results[0].available_quantity > 0;
+};
+
+// Checkout a book (assuming `isBookAvailable` is checked beforehand)
+const checkoutBook = async (bookId, userId, checkoutDate, dueDate) => {
+  const query = `INSERT INTO Checkouts (book_id, user_id, checkout_date, due_date) VALUES (${bookId}, ${userId}, ${checkoutDate}, ${dueDate})`;
+  await executeLibraryDBQuery(query, [bookId, userId, checkoutDate, dueDate]);
+  // Decrement available quantity after successful checkout
+  await updateBook(bookId, null, null, null, -1); // Update only available_quantity
+};
+
 const endConnection = () => {
     // Close the connection
     connection.end((err) => {
@@ -64,5 +146,19 @@ module.exports = {
     getBooks,
     getUsers,
     getCheckouts,
+    deleteAuthor,
+    deleteBook,
+    deleteCheckout,
+    deleteUser,
+    updateAuthor,
+    updateBook,
+    updateCheckout,
+    updateUser,
+    getAuthorById,
+    getBookById,
+    getUserById,
+    getCheckoutById,
+    isBookAvailable,
+    checkoutBook,
     endConnection
 }
--- library-api/server.js
+++ library-api/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAuthors, getBooks, getUsers, getCheckouts } = require("./library_db");
+const { getAuthors, getBooks, getUsers, getCheckouts, getAuthorById, getBookById, getUserById } = require("./library_db");
 
 const app = express();
 
@@ -14,17 +14,37 @@ app.get('/authors', async (req,res)=>{
     res.send({authors});
 })
 
+app.get('/authors/:id', async (req,res)=>{
+    const {id} = req.params
+    let author = await getAuthorById(id);
+    res.send({author})
+})
+
 app.get('/books', async (req,res)=>{
     let books = await getBooks();
     res.send({books});
 })
-
+app.get('/books/:id', async (req,res)=>{
+    const {id} = req.params
+    let book= await getBookById(id);
+    res.send({book});
+})
 app.get('/users', async (req,res)=>{
     let users = await getUsers();
     res.send({users});
 })
+app.get('/users/:id', async (req,res)=>{
+    const {id} = req.params
+    let user = await getUserById(id);
+    res.send({user});
+})
 app.get('/checkouts', async (req,res)=>{
     let checkouts = await getCheckouts();
+    res.send({checkouts});
+})
+app.get('/checkouts/:id', async (req,res)=>{
+    const {id} = req.params
+    let checkouts = await getCheckouts(id);
     res.send({checkouts});
 })
 app.all('*', (req,res) => {
@@ -33,6 +53,10 @@ app.all('*', (req,res) => {
         status_code: 404 
     });
 })
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send({ message: 'Internal Server Error' });
+});
 app.listen(port, () =>{
     console.log(`Listening on port ${port}`)
 })
